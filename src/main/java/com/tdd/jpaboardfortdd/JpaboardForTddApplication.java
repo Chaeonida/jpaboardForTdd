d486deb84926f695cf36ee633f7184bab641939a
Chaeonida <46310555+Chaeonida@users.noreply.github.com>
2022-02-15 19:46:55 +0900

    Initial commit

---
+++ src/main/java/com/tdd/jpaboardfortdd/JpaboardForTddApplication.java
@@ -0,0 +1,16 @@
+package com.tdd.jpaboardfortdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaboardForTddApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(
+				JpaboardForTddApplication.class,
+				args
+		);
+	}
+	
+}
