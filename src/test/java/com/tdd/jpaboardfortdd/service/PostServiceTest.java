524f14603b1794edb1e862a56b76034495528dbd
Chaeonida <46310555+Chaeonida@users.noreply.github.com>
2022-02-15 20:54:52 +0900

    Test : 게시글 등록 시나리오 작성

---
+++ src/test/java/com/tdd/jpaboardfortdd/service/PostServiceTest.java
@@ -0,0 +1,18 @@
+package com.tdd.jpaboardfortdd.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PostServiceTest {
+	
+	@Test
+	@DisplayName("게시글 등록 테스트 ")
+	void createPostTest() {
+		//given(user가 주어졌을때 )
+		
+		//when(user가 게시글을 작성하면)
+		
+		//then(등록이 되어야한다.)
+	}
+}
+
