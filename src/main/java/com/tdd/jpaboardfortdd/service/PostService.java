c5d0406e93558db5c39561feafc088d82d233151
Chaeonida <46310555+Chaeonida@users.noreply.github.com>
2022-02-18 14:37:47 +0900

    Refactor: Transactional readOnly 설정

--- src/main/java/com/tdd/jpaboardfortdd/service/PostService.java
+++ src/main/java/com/tdd/jpaboardfortdd/service/PostService.java
@@ -3,19 +3,20 @@ package com.tdd.jpaboardfortdd.service;
 import com.tdd.jpaboardfortdd.domain.Post;
 import com.tdd.jpaboardfortdd.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
-import javax.transaction.Transactional;
 
-@Transactional
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
 
+    @Transactional
     public Post save(Post post) {
         return postRepository.save(post);
     }
 
+    @Transactional
     public Post update(Long id, String title, String content) {
         Post findPost = postRepository.findById(id).orElseThrow(IllegalArgumentException::new);
 
@@ -23,6 +24,7 @@ public class PostService {
         return findPost;
     }
 
+    @Transactional(readOnly = true)
     public Post find(Long id)  {
         return postRepository.findById(id).orElseThrow(IllegalArgumentException::new);
     }
