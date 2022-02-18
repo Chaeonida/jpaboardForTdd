1c6b985b0c03624f3f2702ae2e6966f581a5f4ae
Chaeonida <46310555+Chaeonida@users.noreply.github.com>
2022-02-18 14:26:04 +0900

    Refactor: 예외처리 IllegalArgumentException으로 변경

--- src/main/java/com/tdd/jpaboardfortdd/service/PostService.java
+++ src/main/java/com/tdd/jpaboardfortdd/service/PostService.java
@@ -4,11 +4,7 @@ import com.tdd.jpaboardfortdd.domain.Post;
 import com.tdd.jpaboardfortdd.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import javax.transaction.Transactional;
-import java.util.Optional;
-
-import static org.springframework.data.crossstore.ChangeSetPersister.*;
 
 @Transactional
 @Service
@@ -20,16 +16,14 @@ public class PostService {
         return postRepository.save(post);
     }
 
-    public Post update(Long id, String title, String content) throws NotFoundException {
-        Post findPost = postRepository.findById(id).orElseThrow(NotFoundException::new);
+    public Post update(Long id, String title, String content) {
+        Post findPost = postRepository.findById(id).orElseThrow(IllegalArgumentException::new);
 
         findPost.updatePost(title, content);
         return findPost;
-
-
     }
 
-    public Post find(Long id) throws NotFoundException {
-        return postRepository.findById(id).orElseThrow(NotFoundException::new);
+    public Post find(Long id)  {
+        return postRepository.findById(id).orElseThrow(IllegalArgumentException::new);
     }
 }
--- src/test/java/com/tdd/jpaboardfortdd/service/PostServiceTest.java
+++ src/test/java/com/tdd/jpaboardfortdd/service/PostServiceTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.data.crossstore.ChangeSetPersister;
 
+import java.util.Optional;
+
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -65,7 +67,7 @@ public class PostServiceTest {
 
         //then(게시글이 조회 되어야 한다.)
         assertThat(findPost.getId(), is(post.getId()));
-        assertThrows(ChangeSetPersister.NotFoundException.class,()-> postService.find(2L));
+        assertThrows(IllegalArgumentException.class,()-> postService.find(2L));
 
     }
 
