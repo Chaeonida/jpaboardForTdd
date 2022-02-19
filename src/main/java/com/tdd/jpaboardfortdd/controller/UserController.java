a4b48c691172b828ce941150b7a32d1ad494206f
Chaeonida <46310555+Chaeonida@users.noreply.github.com>
2022-02-20 01:40:48 +0900

    Feat : User CRUD api 추가

---
+++ src/main/java/com/tdd/jpaboardfortdd/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.tdd.jpaboardfortdd.controller;
+
+import com.tdd.jpaboardfortdd.domain.Post;
+import com.tdd.jpaboardfortdd.dto.PostUpdateRequest;
+import com.tdd.jpaboardfortdd.dto.UserCreateRequest;
+import com.tdd.jpaboardfortdd.dto.UserDetailResponse;
+import com.tdd.jpaboardfortdd.dto.UserUpdateRequest;
+import com.tdd.jpaboardfortdd.service.PostService;
+import com.tdd.jpaboardfortdd.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "users")
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<Long> createUser(
+            @RequestBody UserCreateRequest userCreateRequest
+    ) {
+        return ResponseEntity.ok(userService.save(userCreateRequest));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Long> updateUser(
+            @PathVariable("id") Long userId, @RequestBody UserUpdateRequest userUpdateRequest
+    ) {
+        return ResponseEntity.ok(userService.update(userUpdateRequest,userId));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDetailResponse> get(
+            @PathVariable("id") Long userId
+    ) {
+        return ResponseEntity.ok(userService.get(userId));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Long> delete(
+            @PathVariable("id") Long userId
+    ) {
+        return ResponseEntity.ok(userService.delete(userId));
+    }
+}
