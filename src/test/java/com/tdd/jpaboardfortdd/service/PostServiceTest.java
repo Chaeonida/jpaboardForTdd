7a628df5ef5693e867fef3c9fa162c0f8635040c
Chaeonida <46310555+Chaeonida@users.noreply.github.com>
2022-02-21 18:04:44 +0900

    Test : Test코드 수정

--- src/test/java/com/tdd/jpaboardfortdd/service/PostServiceTest.java
+++ src/test/java/com/tdd/jpaboardfortdd/service/PostServiceTest.java
@@ -2,6 +2,8 @@ package com.tdd.jpaboardfortdd.service;
 
 import com.tdd.jpaboardfortdd.domain.Post;
 import com.tdd.jpaboardfortdd.domain.User;
+import com.tdd.jpaboardfortdd.dto.PostCreateRequest;
+import com.tdd.jpaboardfortdd.dto.PostUpdateRequest;
 import com.tdd.jpaboardfortdd.repository.PostRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,13 +12,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.crossstore.ChangeSetPersister;
 
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
 public class PostServiceTest {
@@ -34,10 +36,10 @@ public class PostServiceTest {
     @DisplayName("게시글 등록 테스트 ")
     void createPostTest() {
         //given(user,Post 가 주어졌을때 )
-        Mockito.when(postRepository.save(post)).thenReturn(post);
+        Mockito.when(postRepository.save(any())).thenReturn(post);
 
         //when(user가 게시글을 작성하면)
-        Post savedPost = postService.save(post);
+        Post savedPost = postService.save(PostCreateRequest);
 
         //then(등록이 되어야한다.)
         assertThat(savedPost.getId(), is(1L));
@@ -45,45 +47,43 @@ public class PostServiceTest {
 
     @Test
     @DisplayName("게시글 수정 테스트 ")
-    void updatePostTest() throws ChangeSetPersister.NotFoundException {
+    void updatePostTest() {
         //given(저장 되어 있는 Post 가 주어졌을때 )
-        Mockito.when(postRepository.findById(post.getId())).thenReturn(java.util.Optional.of(post));
+        Mockito.when(postRepository.findById(post.getId())).thenReturn(Optional.of(post));
 
         //when(게시글을 수정 하면)
-        Post updatedPost = postService.update(post.getId(), "제목수정", "내용수정");
+        Post updatedPost = postService.update(PostUpdateRequest);
 
         //then(게시글이 수정 되어야 한다.)
         assertThat(updatedPost.getContent(), is("내용수정"));
         assertThat(updatedPost.getTitle(), is("제목수정"));
-
     }
 
     @Test
     @DisplayName("게시글 조회 테스트 ")
     void findPostTest() {
         //given(저장 되어 있는 Post 가 주어졌을때 )
-        Mockito.when(postRepository.findById(post.getId())).thenReturn(java.util.Optional.of(post));
+        Mockito.when(postRepository.findById(any())).thenReturn(Optional.of(post));
 
         //when(게시글을 조회 하면)
         Post findPost = postService.find(post.getId());
 
         //then(게시글이 조회 되어야 한다.)
         assertThat(findPost.getId(), is(post.getId()));
-        assertThrows(IllegalArgumentException.class,()-> postService.find(2L));
-
+        assertThrows(IllegalArgumentException.class, () -> postService.find(2L));
     }
 
     @Test
     @DisplayName("게시글 조회 실패 테스트 ")
     void findPostFailTest() {
         //given(저장 되어 있는 Post 가 주어졌을때 )
+        Mockito.when(postRepository.findById(any())).thenReturn(Optional.empty());
 
         //when(게시글을 조회 하면)
         Mockito.when(postRepository.findById(2L)).thenThrow(new IllegalArgumentException());
 
         //then(게시글이 조회 되어야 한다.)
-        assertThrows(IllegalArgumentException.class,()-> postService.find(2L));
-
+        assertThrows(IllegalArgumentException.class, () -> postService.find(2L));
     }
 
     @Test
@@ -96,8 +96,6 @@ public class PostServiceTest {
 
         //then(게시글이 삭제 되어야 한다.)
         assertThat(deletedPostId, is(post.getId()));
-
     }
-
 }
 
